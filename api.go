37e0a72a795bc9a76e5334ffa996c7292b01f764
pedro.vasconcelos <pedrohnvasconcelos@gmail.com>
2024-08-05 19:51:39 -0300

    chore: Add API server implementation with basic endpoints

--- api.go
+++ api.go
@@ -1,9 +1,69 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func writeJson(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			writeJson(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
 type APIServer struct {
 	listenAddr string
 }
 
 func NewAPIServer(listenAddr string) *APIServer {
 	return &APIServer{listenAddr: listenAddr}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount)).Methods("GET")
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleCreateAccount)).Methods("POST")
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccount)).Methods("GET")
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleDeleteAccount)).Methods("DELETE")
+	router.HandleFunc("/account/{id}/transfer", makeHTTPHandleFunc(s.handleTransferAccount)).Methods("POST")
+	log.Println(("Starting server on " + s.listenAddr))
+	http.ListenAndServe(s.listenAddr, router)
+
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleTransferAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
